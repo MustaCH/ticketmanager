256567da4218f6d4d65159276c0a610250204064
Musta <polettiignacio7@gmail.com>
2023-10-26 21:14:52 -0300

    first commit

---
+++ src/components/shared/guestCard/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function GuestCard() {
+  return <div>GuestCard</div>;
+}
+
+export default GuestCard;
---
+++ src/components/shared/index.js
@@ -0,0 +1,4 @@
+export { default as Ticket } from "./ticket/index";
+export { default as UserCard } from "./userCard/index";
+export { default as GuestCard } from "./guestCard/index";
+export { default as Input } from "./input/index";
