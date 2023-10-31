bdc075641602103270e63a9376c335abb7c2e59a
Musta <polettiignacio7@gmail.com>
2023-10-31 08:23:55 -0300

    [feature] - added page in process messege to stats screen

--- src/components/screens/stats/index.jsx
+++ src/components/screens/stats/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { IoConstructSharp } from "react-icons/io5";
 
 function Stats() {
-  return <div className="h-screen">Stats</div>;
+  return (
+    <div className="lg:ps-24 h-screen grid ">
+      <div className="self-center flex flex-col gap-2 items-center">
+        <p className="text-2xl font-bold text-white uppercase">
+          Página en proceso
+        </p>
+        <IoConstructSharp className="text-5xl text-white" />
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
